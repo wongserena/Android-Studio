94ae84dbb7b0afdc37fe8a9dfa1128736d43af5a
matthew visser <m3visser@uwaterloo.ca>
2021-03-06 12:04:05 -0500

    page three

--- lib/Matthew_changes.dart
+++ lib/Matthew_changes.dart
@@ -1,1 +1,18 @@
-//Matthew's changes
+import 'package:flutter/material.dart';
+//Matthew's Changes
+class PageThree extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('Page Three'),
+        ),
+        body: const Center(
+            child: Text(
+              'This is page three',
+              style: TextStyle(fontSize: 24),
+            )
+        )
+    );;
+  }
+}
